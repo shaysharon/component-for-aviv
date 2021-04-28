6bdd28bf388ab58d14096e283d3a09afe8a34392
Shay Sharon <shaysha@google.com>
2021-04-28 20:19:31 +0300

    sample code

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import styles from './index.module.scss';
 
 function App() {
+  const [selected, setSelected] = useState(false);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={styles.app}>
+      <div className={styles.title}>Title of app component</div>
+      <div className={`${styles.card} ${selected ? styles.selected : ''}`} onClick={() => setSelected(!selected)}>
+        <div className={styles.title}>Title of card</div>
+      </div>
     </div>
   );
 }
